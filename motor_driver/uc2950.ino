890391ea07bb0df5e2fcf1633c71c67829f64141
Pritam Soni <pritamsoni.hsr@gmail.com>
2018-04-01 16:10:25 +0530

    bidirectional rx speed

--- motor_driver/uc2950.ino
+++ motor_driver/uc2950.ino
@@ -1,197 +1,164 @@
-#include <avr/io.h> 
+#include <avr/io.h>
 #include <avr/interrupt.h>
-int pwm_value=0;
-volatile int a=0;
-volatile int flag=25;
-volatile uint32_t Ticks=0;
-volatile uint32_t Ticks_new=0;
-volatile uint32_t temp=0;
-volatile int ovf=0;
-volatile float dt=0.004096;
-volatile int Error=0;
-volatile int Prev_Error=0;
-volatile int Intg=0;
-volatile uint32_t Target_Ticks=315;
-float Kp=0.28;
-float Ki=0;
-float Kd=0.0035;
-float pid=0;
-int Hall1=21;//Pin-21
-int Hall2=20;//Pin-20
-int Hall3=19;//Pin-19
-int PmosA=4;
-int ENA=5;
-int PmosB=6;
-int ENB=7;
-int PmosC=8;
-int ENC=9;
-int drxn=17;
+int a=0,flag=1;
+volatile int ovf=0,Error=0,Prev_Error=0,pwm_value=0;
+volatile uint16_t Ticks_new=0,Ticks=0,temp=0,Target_Ticks=315;
+volatile float Intg=0;
+float Kp=0.28,Ki=0,Kd=0.0035,pid=0,dt=0.004096;
+int Hall1=21,Hall2=20,Hall3=19;
+int S1=4,S2=5,S3=6,S4=7,S5=8,S6=9;
+bool direction=true;    //1-cw,0-ccw
 void Stop(){
-  analogWrite(PmosA,255);
-  analogWrite(PmosB,255);
-  analogWrite(PmosC,255);
-  analogWrite(ENA,0);
-  analogWrite(ENB,0);
-  analogWrite(ENC,0);
+  analogWrite(S1,255);
+  analogWrite(S3,255);
+  analogWrite(S5,255);
+  analogWrite(S2,0);
+  analogWrite(S4,0);
+  analogWrite(S6,0);
 }
 volatile int HallValue(){
-  return((digitalRead(Hall1))+(digitalRead(Hall2)*2)+(digitalRead(Hall3)*4));//Bases are 1,2,4.   
+  return((digitalRead(Hall1))+(digitalRead(Hall2)*2)+(digitalRead(Hall3)*4));//Bases are 1,2,4.
 }
 void Check_PWM_CW(volatile int HallValue){
   switch(HallValue){
-   case 0:    //+. +. +
-   {
+   case 0: {
      Stop();
      break;
    }
-   case 3:    //0. +. -
-   {
-     analogWrite(PmosA,0);
-     analogWrite(ENA,pwm_value);
-     analogWrite(PmosB,pwm_value);
-     analogWrite(ENB,0);
-     analogWrite(PmosC,0);
-     analogWrite(ENC,0);
+   case 3: {                                      //0. +. -
+     analogWrite(S1,0);
+     analogWrite(S2,255);
+     analogWrite(S3,pwm_value);
+     analogWrite(S4,0);
+     analogWrite(S5,0);
+     analogWrite(S6,0);
      break;
    }
-   case 2:   //-. +. 0
-    {
-     analogWrite(PmosA,0);
-     analogWrite(ENA,0);
-     analogWrite(PmosB,pwm_value);
-     analogWrite(ENB,0);
-     analogWrite(PmosC,0);
-     analogWrite(ENC,pwm_value);
+   case 2: {                                      //-. +. 0
+     analogWrite(S1,0);
+     analogWrite(S2,0);
+     analogWrite(S3,pwm_value);
+     analogWrite(S4,0);
+     analogWrite(S5,0);
+     analogWrite(S6,255);
      break;
     }
-   case 6:    //-. 0. +
-    {
-     analogWrite(PmosA,0);
-     analogWrite(ENA,0);
-     analogWrite(PmosB,0);
-     analogWrite(ENB,pwm_value);
-     analogWrite(PmosC,pwm_value);
-     analogWrite(ENC,0);
+   case 6: {                                      //-. 0. +
+     analogWrite(S1,0);
+     analogWrite(S2,0);
+     analogWrite(S3,0);
+     analogWrite(S4,255);
+     analogWrite(S5,pwm_value);
+     analogWrite(S6,0);
      break;
     }
-   case 4:    //0. -. +
-   {
-     analogWrite(PmosA,0);
-     analogWrite(ENA,pwm_value);
-     analogWrite(PmosB,0);
-     analogWrite(ENB,0);
-     analogWrite(PmosC,pwm_value);
-     analogWrite(ENC,0);
+   case 4: {                                      //0. -. +
+     analogWrite(S1,0);
+     analogWrite(S2,255);
+     analogWrite(S3,0);
+     analogWrite(S4,0);
+     analogWrite(S5,pwm_value);
+     analogWrite(S6,0);
      break;
    }
-   case 5:    //+. -. 0
-   {
-     analogWrite(PmosA,pwm_value);
-     analogWrite(ENA,0);
-     analogWrite(PmosB,0);
-     analogWrite(ENB,0);
-     analogWrite(PmosC,0);
-     analogWrite(ENC,pwm_value);
+   case 5: {                                      //+. -. 0
+     analogWrite(S1,pwm_value);
+     analogWrite(S2,0);
+     analogWrite(S3,0);
+     analogWrite(S4,0);
+     analogWrite(S5,0);
+     analogWrite(S6,255);
      break;
     }
-  case 1:   //+. 0. -
-  {
-     analogWrite(PmosA,pwm_value);
-     analogWrite(ENA,0);
-     analogWrite(PmosB,0);
-     analogWrite(ENB,pwm_value);
-     analogWrite(PmosC,0);
-     analogWrite(ENC,0);
+  case 1: {                                       //+. 0. -
+     analogWrite(S1,pwm_value);
+     analogWrite(S2,0);
+     analogWrite(S3,0);
+     analogWrite(S4,255);
+     analogWrite(S5,0);
+     analogWrite(S6,0);
      break;
    }
-  case 7:
-  {
+  case 7:{
    Stop();
    break;
-  }  
+  }
  }
 }
 void Check_PWM_CCW(volatile int HallValue){
   switch(HallValue){
-   case 0:    //+. +. +
-   {
+   case 0: {
      Stop();
      break;
    }
-   case 3:    //0. +. -
-   {
-     analogWrite(PmosA,0);
-     analogWrite(ENA,pwm_value);
-     analogWrite(PmosB,0);
-     analogWrite(ENB,0);
-     analogWrite(PmosC,pwm_value);
-     analogWrite(ENC,0);
+   case 3: {
+     analogWrite(S1,0);
+     analogWrite(S2,255);
+     analogWrite(S3,0);
+     analogWrite(S4,0);
+     analogWrite(S5,pwm_value);
+     analogWrite(S6,0);
      break;
    }
-   case 2:   //-. +. 0
-    {
-     analogWrite(PmosA,pwm_value);
-     analogWrite(ENA,0);
-     analogWrite(PmosB,0);
-     analogWrite(ENB,0);
-     analogWrite(PmosC,0);
-     analogWrite(ENC,pwm_value);
+   case 2: {
+     analogWrite(S1,pwm_value);
+     analogWrite(S2,0);
+     analogWrite(S3,0);
+     analogWrite(S4,0);
+     analogWrite(S5,0);
+     analogWrite(S6,255);
      break;
     }
-   case 6:    //-. 0. +
-    {
-     analogWrite(PmosA,pwm_value);
-     analogWrite(ENA,0);
-     analogWrite(PmosB,0);
-     analogWrite(ENB,pwm_value);
-     analogWrite(PmosC,0);
-     analogWrite(ENC,0);
+   case 6: {
+     analogWrite(S1,pwm_value);
+     analogWrite(S2,0);
+     analogWrite(S3,0);
+     analogWrite(S4,255);
+     analogWrite(S5,0);
+     analogWrite(S6,0);
      break;
     }
-   case 4:    //0. -. +
-   {
-     analogWrite(PmosA,0);
-     analogWrite(ENA,pwm_value);
-     analogWrite(PmosB,pwm_value);
-     analogWrite(ENB,0);
-     analogWrite(PmosC,0);
-     analogWrite(ENC,0);
+   case 4: {
+     analogWrite(S1,0);
+     analogWrite(S2,255);
+     analogWrite(S3,pwm_value);
+     analogWrite(S4,0);
+     analogWrite(S5,0);
+     analogWrite(S6,0);
      break;
    }
-   case 5:    //+. -. 0
-   {
-     analogWrite(PmosA,0);
-     analogWrite(ENA,0);
-     analogWrite(PmosB,pwm_value);
-     analogWrite(ENB,0);
-     analogWrite(PmosC,0);
-     analogWrite(ENC,pwm_value);
+   case 5: {
+     analogWrite(S1,0);
+     analogWrite(S2,0);
+     analogWrite(S3,pwm_value);
+     analogWrite(S4,0);
+     analogWrite(S5,0);
+     analogWrite(S6,255);
      break;
     }
-  case 1:   //+. 0. -
-  {
-     analogWrite(PmosA,0);
-     analogWrite(ENA,0);
-     analogWrite(PmosB,0);
-     analogWrite(ENB,pwm_value);
-     analogWrite(PmosC,pwm_value);
-     analogWrite(ENC,0);
+  case 1: {
+     analogWrite(S1,0);
+     analogWrite(S2,0);
+     analogWrite(S3,0);
+     analogWrite(S4,255);
+     analogWrite(S5,pwm_value);
+     analogWrite(S6,0);
      break;
    }
   case 7:
   {
    Stop();
    break;
-  }  
+  }
  }
 }
 void Init_Pins(){
-  pinMode(PmosA,OUTPUT);
-  pinMode(ENA,OUTPUT);
-  pinMode(PmosB,OUTPUT);
-  pinMode(ENB,OUTPUT);
-  pinMode(PmosC,OUTPUT);
-  pinMode(ENC,OUTPUT);
+  pinMode(S1,OUTPUT);
+  pinMode(S2,OUTPUT);
+  pinMode(S3,OUTPUT);
+  pinMode(S4,OUTPUT);
+  pinMode(S5,OUTPUT);
+  pinMode(S6,OUTPUT);
   pinMode(Hall1,INPUT);
   pinMode(Hall2,INPUT);
   pinMode(Hall3,INPUT);
@@ -207,40 +174,37 @@ void Init_Interrupt(){
   EICRA=(1<<ISC00)|(1<<ISC10)|(1<<ISC20)|(1<<ISC30);
 }
 ISR(INT0_vect){
-    if (digitalRead(drxn)==0)
+    if (direction==1)
       Check_PWM_CW(HallValue());
-
-  if (digitalRead(drxn)==1)
+    if (direction==0)
       Check_PWM_CCW(HallValue());
-  digitalWrite(LED_BUILTIN,!(digitalRead(LED_BUILTIN)));
 }
 ISR(INT1_vect){
-    if (digitalRead(drxn)==0)
+    if (direction==1)
       Check_PWM_CW(HallValue());
-
-  if (digitalRead(drxn)==1)
+    if (direction==0)
       Check_PWM_CCW(HallValue());
 }
 ISR(INT2_vect){
-  if (digitalRead(drxn)==0)
+    if (direction==1)
       Check_PWM_CW(HallValue());
-
-  if (digitalRead(drxn)==1)
+    if (direction==0)
       Check_PWM_CCW(HallValue());
 }
 ISR(INT3_vect){
-  Ticks++; 
+    Ticks++;
 }
 ISR(TIMER5_OVF_vect){
   Ticks_new=Ticks-temp;
   temp=Ticks;
   flag=1;
-  PID(); 
+  PID();
 }
 void setup() {
   Serial.begin(9600);
+  int myTimeout = 1;  // milliseconds for Serial.read
+  Serial.setTimeout(myTimeout);
   Init_Pins();
-  pinMode(LED_BUILTIN,OUTPUT);
   Init_Interrupt();
   TCCR5A=0;
   TCCR5B=(1<<CS50);//Prescaler=0.
@@ -249,6 +213,12 @@ void setup() {
   ovf=0;
 }
 void loop(){
+
+  while(stream.available()){
+    a=stream.read();
+    b=a.toInt();
+  }
+
   if(flag==1){
     Serial.println(Ticks_new);
     flag=0;
@@ -258,7 +228,6 @@ void PID(){
   Error=Target_Ticks-Ticks_new;
   Intg=Intg+Error*dt;
   pid=pid+(Kp*Error)+(Kd*(Error-Prev_Error)/dt)+Ki*Intg;
-  //a=1;
   if(pid>255){
     pid=255;
   }
@@ -266,6 +235,5 @@ void PID(){
     pid=0;
   }
    Prev_Error=Error;
-   //pid=(pid*255)/310;
    pwm_value=pid;
 }
